73bd6d55cc05947afe0804bcb315ebe939785fac
Gregor Middell <gregor@middell.net>
2022-02-15 16:01:58 +0100

    Adds test case generating a coverage report of the compiled transducer

---
+++ tests/test_coverage.py
@@ -0,0 +1,40 @@
+import csv
+
+import dwdsmor
+
+
+def test_wb_coverage(project_dir, dwdsmor_transducer, wb_entries):
+    coverage_report_file = project_dir / 'wb-coverage-report.csv'
+    with coverage_report_file.open('w') as report_f:
+        report_writer = csv.writer(report_f)
+        report_writer.writerow([
+            "Datei",
+            "Status",
+            "Lemma",
+            "Wortklasse",
+            "DWDSmor-Lemma",
+            "DWDSmor-POS",
+            "Abdeckung"
+        ])
+        for wb_n, wb_entry in enumerate(wb_entries):
+            lemma = wb_entry['written_repr']
+            analyses = dwdsmor_transducer.analyse(lemma)
+            analyses = dwdsmor.analysis.parse(analyses)
+            analyzed = False
+            analyzed_lemma = ''
+            analyzed_pos = ''
+            for analysis in analyses:
+                if analysis.lemma == lemma:
+                    analyzed = True
+                    analyzed_lemma = analysis.lemma or ''
+                    analyzed_pos = analysis.pos or ''
+                    break
+            report_writer.writerow([
+                wb_entry['file'].as_posix(),
+                wb_entry['article_status'],
+                lemma,
+                wb_entry['pos'],
+                analyzed_lemma,
+                analyzed_pos,
+                ("1" if analyzed else "0")
+            ])
