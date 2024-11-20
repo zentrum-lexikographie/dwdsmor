6743ac8a630837aa51d293b1b4a93a5feecd87e2
Gregor Middell <gregor@middell.net>
2024-11-20 13:10:18 +0100

    Models markers/tags in dwdsmor.tag

---
+++ dwdsmor/tag.py
@@ -0,0 +1,130 @@
+lexeme_tags = ("lidx", "pidx", "pos")
+inflection_tags = (
+    "category",
+    "degree",
+    "function",
+    "person",
+    "gender",
+    "case",
+    "number",
+    "nonfinite",
+    "tense",
+    "mood",
+    "auxiliary",
+    "inflection",
+)
+info_tags = ("metainfo", "orthinfo", "charinfo", "ellipinfo")
+wordformation_tags = ("processes", "means")
+
+all_tags = (*lexeme_tags, *inflection_tags, *info_tags, *wordformation_tags)
+
+tag_values = {
+    "": {
+        "lidx": {f"IDX{i}" for i in range(1, 9)},
+        "pidx": {f"PAR{i}" for i in range(1, 9)},
+        "pos": {
+            "+ADJ",
+            "+ADV",
+            "+ART",
+            "+CARD",
+            "+CONJ",
+            "+DEM",
+            "+FRAC",
+            "+INDEF",
+            "+INTJ",
+            "+NN",
+            "+NPROP",
+            "+ORD",
+            "+POSS",
+            "+POSTP",
+            "+PPRO",
+            "+PREP",
+            "+PREPART",
+            "+PROADV",
+            "+PTCL",
+            "+PUNCT",
+            "+REL",
+            "+V",
+            "+WPRO",
+        },
+        "degree": {"Pos", "Comp", "Sup"},
+        "function": {"Attr", "Subst"},
+        "person": {"1", "2", "3"},
+        "gender": {"Fem", "Neut", "Masc", "NoGend"},
+        "case": {"Nom", "Gen", "Dat", "Acc"},
+        "number": {"Sg", "Pl"},
+        "nonfinite": {"Inf", "Part"},
+        "tense": {"Pres", "Past", "Perf"},
+        "mood": {"Ind", "Subj", "Imp"},
+        "auxiliary": {"haben", "sein"},
+        "inflection": {"St", "Wk", "NoInfl"},
+        "metainfo": {"Old", "NonSt"},
+        "orthinfo": {"OLDORTH", "CH"},
+        "charinfo": {"CAP"},
+        "ellipinfo": {"TRUNC"},
+        "processes": {"COMP", "DER", "CONV"},
+        "means": {"concat", "hyph", "ident", "pref", "prev", "suff"},
+    },
+    "+ADJ": {"function": {"Attr", "Subst", "Attr/Subst", "Pred/Adv"}},
+    "+ART": {
+        "category": {"Def", "Indef", "Neg"},
+    },
+    "+CONJ": {"category": {"AdjComp", "Coord", "Sub", "InfCl"}},
+    "+ORD": {
+        "function": {"Attr/Subst"},
+    },
+    "+POSS": {
+        "function": {"Attr/Subst"},
+    },
+    "+PPRO": {
+        "category": {"Pers", "Refl", "Rec"},
+    },
+    "+PTCL": {"category": {"AdjPos", "AdjSup", "InfCl", "Neg"}},
+    "+PUNCT": {
+        "category": {
+            "Comma",
+            "Period",
+            "Ellip",
+            "Quote",
+            "Paren",
+            "Dash",
+            "Slash",
+            "Other",
+        },
+    },
+    "+V": {"category": {"NonCl", "Cl"}},
+}
+
+
+tag_types = {
+    v: tt for _pos, tvs in tag_values.items() for tt, vs in tvs.items() for v in vs
+}
+
+
+inflection_boundary_tags = "~"
+word_formation_tags = "#-|"
+other_boundary_tags = "="
+boundary_tags = inflection_boundary_tags + word_formation_tags + other_boundary_tags
+
+inflection_tag_seqs = {
+    "": (("function", "gender", "case", "number", "inflection"),),
+    "+ADJ": (("degree", "function", "gender", "case", "number", "inflection"),),
+    "+ADV": (("degree"),),
+    "+ART": (("category", "function", "gender", "case", "number", "inflection"),),
+    "+CONJ": (("category",),),
+    "+FRAC": (("function",),),
+    "+INTJ": ((),),
+    "+NN": (("gender", "case", "number", "inflection"),),
+    "+NPROP": (("gender", "case", "number", "inflection"),),
+    "+PPRO": (("category", "person", "gender", "case", "number"),),
+    "+PREP": ((),),
+    "+PREPART": (("gender", "case", "inflection"),),
+    "+PROADV": ((),),
+    "+PTCL": (("category"),),
+    "+V": (
+        ("mood", "number"),
+        ("nonfinite", "category"),
+        ("nonfinite", "tense", "auxiliary"),
+        ("person", "number", "tense", "mood"),
+    ),
+}
--- dwdsmor/traversal.py
+++ dwdsmor/traversal.py
@@ -1,89 +1,6 @@
 from dataclasses import dataclass
 
-tags = {
-    "lidx": {f"IDX{i}" for i in range(1, 9)},
-    "pidx": {f"PAR{i}" for i in range(1, 9)},
-    "pos": {
-        "+ADJ",
-        "+ADV",
-        "+ART",
-        "+CARD",
-        "+CONJ",
-        "+DEM",
-        "+FRAC",
-        "+INDEF",
-        "+INTJ",
-        "+NN",
-        "+NPROP",
-        "+ORD",
-        "+POSS",
-        "+POSTP",
-        "+PPRO",
-        "+PREP",
-        "+PREPART",
-        "+PROADV",
-        "+PTCL",
-        "+PUNCT",
-        "+REL",
-        "+V",
-        "+WPRO",
-    },
-    "gender": {"Fem", "Neut", "Masc", "NoGend", "Invar"},
-    "number": {"Sg", "Pl", "Invar"},
-    "case": {"Nom", "Gen", "Dat", "Acc", "Invar"},
-    "degree": {"Pos", "Comp", "Sup"},
-    "function": {
-        "Attr",
-        "Subst",
-        "Attr/Subst",
-        "Pred/Adv",
-        "Cl",
-        "NonCl",
-        "Invar",
-    },
-    "tense": {"Pres", "Past", "Perf", "Invar"},
-    "mood": {"Ind", "Subj", "Imp", "Invar"},
-    "person": {"1", "2", "3", "Invar"},
-    "auxiliary": {"haben", "sein"},
-    "inflection": {"St", "Wk", "NoInfl", "Invar"},
-    "nonfinite": {"Inf", "Part", "Invar"},
-    "means": {"concat", "hyph", "ident", "pref", "prev", "suff"},
-    "metainfo": {"Old", "NonSt"},
-    "orthinfo": {"OLDORTH", "CH"},
-    "ellipinfo": {"TRUNC"},
-    "charinfo": {"CAP"},
-    "processes": {"COMP", "DER", "CONV"},
-    "subcat": {
-        "Pers",
-        "Refl",
-        "Rec",
-        "Def",
-        "Indef",
-        "Neg",
-        "Coord",
-        "Sub",
-        "InfCl",
-        "AdjPos",
-        "AdjComp",
-        "AdjSup",
-        "Comma",
-        "Period",
-        "Ellip",
-        "Quote",
-        "Paren",
-        "Dash",
-        "Slash",
-        "Other",
-    },
-}
-
-
-tag_types = {v: k for k, vs in tags.items() for v in vs}
-
-inflection_tags = "~"
-word_formation_tags = "#-|"
-other_boundary_tags = "="
-boundary_tags = inflection_tags + word_formation_tags + other_boundary_tags
+from dwdsmor.tag import boundary_tags, tag_types
 
 
 @dataclass
@@ -91,27 +8,27 @@ class Traversal:
     spec: str
     surface: str
     analysis: str
-    pos: str | None = None
-    gender: str | None = None
-    number: str | None = None
-    case: str | None = None
-    degree: str | None = None
-    function: str | None = None
-    tense: str | None = None
-    mood: str | None = None
-    person: str | None = None
-    auxiliary: str | None = None
-    inflection: str | None = None
-    nonfinite: str | None = None
-    means: str | None = None
-    metainfo: str | None = None
-    orthinfo: str | None = None
-    ellipinfo: str | None = None
-    charinfo: str | None = None
-    processes: str | None = None
-    subcat: str | None = None
     lidx: str | None = None
     pidx: str | None = None
+    pos: str | None = None
+    category: str | None = None
+    degree: str | None = None
+    function: str | None = None
+    person: str | None = None
+    gender: str | None = None
+    case: str | None = None
+    number: str | None = None
+    nonfinite: str | None = None
+    tense: str | None = None
+    mood: str | None = None
+    auxiliary: str | None = None
+    inflection: str | None = None
+    metainfo: str | None = None
+    orthinfo: str | None = None
+    charinfo: str | None = None
+    ellipinfo: str | None = None
+    processes: str | None = None
+    means: str | None = None
 
     def reparse(self, **args):
         return Traversal.parse(self.spec, **args)
