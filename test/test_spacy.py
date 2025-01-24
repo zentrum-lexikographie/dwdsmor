e679773bfb6f89983206c82c0bba30085b7f2f18
Gregor Middell <gregor@middell.net>
2025-01-24 16:49:19 +0100

    chore: Snapshot spaCy test with Open Edition

--- test/test_spacy.py
+++ test/test_spacy.py
@@ -2,8 +2,7 @@ import spacy
 from datasets import load_dataset
 from pytest import fixture
 
-import dwdsmor
-import dwdsmor.spacy
+import dwdsmor.spacy  # noqa
 
 
 @fixture(scope="module")
@@ -11,11 +10,6 @@ def nlp():
     nlp = spacy.load("de_hdt_lg")
     nlp.add_pipe("dwdsmor")
     return nlp
-
-
-@fixture(scope="module")
-def lemmatizer():
-    return dwdsmor.lemmatizer()
 
 
 @fixture(scope="module")
@@ -29,7 +23,7 @@ def sentences():
     return tuple(s["text"] for s in ds.select(range(100)))
 
 
-def test_lemmatisation(nlp, lemmatizer, sentences, snapshot):
+def test_lemmatisation(nlp, sentences, snapshot):
     sentences = sentences[:10]
     docs = nlp.pipe(sentences)
     tokens = ((t.text, t.tag_, t.lemma_, t._.dwdsmor_lemma) for d in docs for t in d)
