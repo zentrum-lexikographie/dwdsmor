b42ca9166dd8c1d393ec7db1aba9c4b541890e47
Luise Köhler <luise.koehler@bbaw.de>
2025-04-03 12:33:09 +0200

    refactor: move logging configuration to function

--- dwdsmor/log.py
+++ dwdsmor/log.py
@@ -2,13 +2,11 @@ import logging
 import os
 import sys
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    stream=sys.stderr,
-    level=(logging.DEBUG if os.getenv("DEBUG") else logging.INFO),
-)
 
-logger = logging.getLogger("dwdsmor")
-
-__all__ = ["logger"]
+def configure_logging():
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        stream=sys.stderr,
+        level=(logging.DEBUG if os.getenv("DEBUG") else logging.INFO),
+    )
